b4f2a81b2fcc75a84d0a592f703bcbb1f8f222ca
RubysLittlestGem <RubysLittlestGem@gmail.com>
2022-09-27 20:06:19 -0700

    initial Commit. add code for lesson 2 calculator and pseudo-code

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,64 @@
+# asks for two numbers
+# asks for the type of operation to perform: add, subtract, multiply or divide
+# performs the operation on the specified arguments
+# displays the result
+
+# answer = Kernel.gets()
+# Kernelputs(answer)
+
+
+Kernel.puts("Welcome to Calculator!")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What's the second number?") 
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What's the operation to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == "1"
+  result = number1.to_i() + number2.to_i()
+elsif operator == "2"
+  result = number1.to_i() - number2.to_i()
+elsif operator == "3"
+  result = number1.to_i() * number2.to_i()
+elsif operator == "4"
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}")
+
+
+
+
+
+
+
+
+#WORK I DID BEFORE WATCHING THE VIDEO
+
+# Kernel.puts("Give me a number")
+# number1 = Kernel.gets().chomp().to_i()
+
+# Kernel.puts("Give me anoither number")
+# number2 = Kernel.gets().chomp().to_i()
+
+# Kernel.puts("What type of operation would you like me to run?")
+# Kernel.puts("")
+# Kernel.puts("Select an option: \n1. Add \n2. Subtract \n3. Multiply \n4. Divide")
+# operation = Kernel.gets().to_i()
+
+# case operation 
+# when 1
+#   puts number1 + number2
+# when 2
+#   puts number1 - number2
+# when 3
+#   puts number1 * number2
+# when 4
+#   puts number1 / number2
+# else
+#  puts "ERROR: Something went wrong!"
+# end
---
+++ lesson_2/pseudo-code.rb
@@ -0,0 +1,20 @@
+# ------------------------------------------
+# a method that returns the sum of two integers
+
+# Given 2 initial Integers.
+# add them together and display the results.
+
+# START
+# SET number1 = 1
+# SET number2 = 2 
+# SET results = number1 + number2
+# PRINT results
+# END
+
+ def sum(num1, num2)
+   puts num1 + num2
+ end
+
+ sum(3, 4)
+
+
